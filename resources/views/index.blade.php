09296be73d29eb86d7ee66ca78954094cee4bc0b
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-09-20 17:11:55 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -102,7 +102,7 @@ let landingSearchDT = {
 					<div class="row">
 						<div class="col-lg-12 col-md-12">
 							<div class="sec-heading center">
-								<h2>Latest Apartments</h2>
+								<h2>Latest Postings</h2>
 							</div>
 						</div>
 					</div>
@@ -110,12 +110,7 @@ let landingSearchDT = {
 					<div class="row">
 						<?php
 						 $popularApartmentss = [
-						   ['img' => asset('img/des-2.jpg'),'href' => "javascript-void(0)", 'tc' => "5",'location' => "Ikeja, Lagos",'stars' => "5", 'amount' => "20000"],
-						   ['img' => asset('img/des-3.jpg'),'href' => "javascript-void(0)", 'tc' => "6",'location' => "Ikorodu, Lagos",'stars' => "3", 'amount' => "10000"],
-						   ['img' => asset('img/des-4.jpg'),'href' => "javascript-void(0)", 'tc' => "5",'location' => "Victoria Island, Lagos",'stars' => "5", 'amount' => "20000"],
-						   ['img' => asset('img/des-5.jpg'),'href' => "javascript-void(0)", 'tc' => "5",'location' => "Bodija, Oyo",'stars' => "3", 'amount' => "7000"],
-						   ['img' => asset('img/des-6.jpg'),'href' => "javascript-void(0)", 'tc' => "7",'location' => "Mokola, Ibadan",'stars' => "4", 'amount' => "10000"],
-						   ['img' => asset('img/des-7.jpg'),'href' => "javascript-void(0)", 'tc' => "7",'location' => "Yaba, Lagos",'stars' => "4", 'amount' => "10000"],
+						   
 						 ];
 						 
 						 foreach($popularApartments as $pa)
@@ -191,11 +186,7 @@ $pt['name'] = $pa['name'];
 								
 								<?php
 								$fa = [
-								  ['name' => "Fravio Apartment",'rating' => "4",'location' => "Abuja, FCT",'likes' => "50",'img' => asset('img/featured/fravio-apartment.jpg')],
-								  ['name' => "A20 Apartment",'rating' => "5",'location' => "Lekki, Lagos",'likes' => "88",'img' => asset('img/featured/a20-apartment.png')],
-								  ['name' => "Williams Courtyard",'rating' => "4",'location' => "Ikeja, Lagos",'likes' => "31",'img' => asset('img/featured/williams-courtyard.jpg')],
-								  ['name' => "Lovitoz Place",'rating' => "5",'location' => "Sapele, Delta",'likes' => "26",'img' => asset('img/featured/lovitoz-place.jpg')],
-								  ['name' => "Topmost Apartment",'rating' => "4",'location' => "Abuja, FCT",'likes' => "43",'img' => asset('img/featured/topmost-apartment.jpg')]
+								  
 								];
 								
 								 foreach($fa as $f)
@@ -232,85 +223,6 @@ $pt['name'] = $pa['name'];
 			</section>
 			<!-- ========================= End Featured Apartments Section ============================ -->
 			
-			
-										<!-- ================= Apartments start ========================= -->
-			<section class="min">
-				<div class="container">
-				
-					<div class="row">
-						<div class="col-lg-12 col-md-12">
-							<div class="sec-heading center">
-								<h2>Law School Apartments</h2>
-							</div>
-						</div>
-					</div>
-					
-					<div class="row">
-						<?php
-						 $popularApartmentss = [
-						   ['img' => asset('img/des-2.jpg'),'href' => "javascript-void(0)", 'tc' => "5",'location' => "Ikeja, Lagos",'stars' => "5", 'amount' => "20000"],
-						   ['img' => asset('img/des-3.jpg'),'href' => "javascript-void(0)", 'tc' => "6",'location' => "Ikorodu, Lagos",'stars' => "3", 'amount' => "10000"],
-						   ['img' => asset('img/des-4.jpg'),'href' => "javascript-void(0)", 'tc' => "5",'location' => "Victoria Island, Lagos",'stars' => "5", 'amount' => "20000"],
-						   ['img' => asset('img/des-5.jpg'),'href' => "javascript-void(0)", 'tc' => "5",'location' => "Bodija, Oyo",'stars' => "3", 'amount' => "7000"],
-						   ['img' => asset('img/des-6.jpg'),'href' => "javascript-void(0)", 'tc' => "7",'location' => "Mokola, Ibadan",'stars' => "4", 'amount' => "10000"],
-						   ['img' => asset('img/des-7.jpg'),'href' => "javascript-void(0)", 'tc' => "7",'location' => "Yaba, Lagos",'stars' => "4", 'amount' => "10000"],
-						 ];
-						 
-						 foreach($popularApartments as $pa)
-						 {
-							 $pt = [];
-$adata = $pa['data'];
-$terms = $pa['terms'];
-$address = $pa['address'];
-$cmedia = $pa['cmedia'];
-$imgs = $cmedia['images'];
-
-$pt['img'] = $imgs[0];
-$pt['href'] = url('apartment')."?xf=".$pa['url'];
-$pt['tc'] = $terms['max_adults'];
-$pt['location'] = $address['city'].", ".$address['state'];
-$pt['stars'] = $pa['rating'];
-$pt['amount'] = $adata['amount'];
-$pt['name'] = $pa['name'];
-						?>
-						<!-- Single Tour Place -->
-						<div class="col-lg-4 col-md-6 col-sm-12">
-							<div class="tour-simple-wrap">
-								<div class="tour-simple-thumb">
-									<a href="{{$pt['href']}}"><img src="{{$pt['img']}}" class="img-fluid img-responsive" alt="{{$pt['name']}}" style="width: 348px; height: 237px;"/></a>
-								</div>
-								<div class="tour-simple-caption">
-									<div class="ts-caption-left">
-										<h4 class="ts-title">
-										 <a href="{{$pt['href']}}">{{$pt['name']}}</a><br>
-										 <a href="javascript:void(0)">{{$pt['location']}}</a>
-										</h4>
-										<span>{{$pt['tc']}} adults max.</span>
-									</div>
-									<div class="ts-caption-right">
-										<div class="ts-caption-rating">
-										   @for($i = 0; $i < $pt['stars']; $i++)
-											<i class="ti-star filled"></i>
-										   @endfor
-										   @for($i = 0; $i < 5 - $pt['stars']; $i++)
-											<i class="ti-star"></i>
-										   @endfor
-										</div>
-										<h5 class="ts-price">&#8358;{{number_format($pt['amount'],2)}}</h5>
-									</div>
-								</div>
-							</div>
-						</div>
-						<?php
-						 }
-						?>
-						
-						
-					</div>
-				
-				</div>
-			</section>
-			<!-- ========================= End Apartment Section ============================ -->
 
       @include('recent-blog')
       @include('newsletter-cta')
