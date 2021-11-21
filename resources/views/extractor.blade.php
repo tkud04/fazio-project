8152e7a1a91c8f3e80aa0790ef910c3f9477a32a
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-11-21 20:18:50 +0100

    Update extractor.blade.php

--- resources/views/extractor.blade.php
+++ resources/views/extractor.blade.php
@@ -10,7 +10,7 @@ $noFooter = true;
   <div class="col-md-12 mb-5">
     <h5>Paste the web page source or any text that contains the leads you want to extract in the box below</h5>
     <div class="form-group">
-      <textarea class="form-control" rows="15" name="xf" placeholder="Paste text here"></textarea>
+      <textarea class="form-control" rows="15" cols="20" name="xf" placeholder="Paste text here"></textarea>
     </div>
     <a href="extract-btn" class="btn btn-primary">Extract</a>
   </div>
