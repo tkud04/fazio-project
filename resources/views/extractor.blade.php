74b4b79e1578b1a8512061e95f25106ebaf878ff
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-11-21 19:45:27 +0100

    Update extractor.blade.php

--- resources/views/extractor.blade.php
+++ resources/views/extractor.blade.php
@@ -9,7 +9,7 @@
     <div class="form-group">
       <textarea class="form-control" rows="15" name="xf" placeholder="Paste text here"></textarea>
     </div>
-    <a href="extract-btn" class="btn btn-primary>Extract</a>
+    <a href="extract-btn" class="btn btn-primary">Extract</a>
   </div>
   <div class="col-md-12 mb-5">
     <h5>Results will be displayed here</h5>
