2e4d37e1b95afd17364a8e4c1dd97cab7295a760
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-11-22 15:29:52 +0100

    Update mmm.js

--- public/js/mmm.js
+++ public/js/mmm.js
@@ -37,7 +37,10 @@ $(document).ready(function() {
 		}
 		
 		else{
-			 extract(text);
+			let emr = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			let matches = text.match(emr);
+			$('#result').html(matches);
+			// extract(text);
 		}
 		
 			
